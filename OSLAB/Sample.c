d76c4a6d13bcc2621f05df5249eb8f0973aa93be
204G1A0562 <94514467+204G1A0562@users.noreply.github.com>
2022-08-30 11:53:40 +0530

    Sample.c
    muheet Completed Sample.c with C source Code

---
+++ OSLAB/Sample.c
@@ -0,0 +1,11 @@
+/*
+Name Of the Candidate: T.Muheet Ur Rahman
+   Roll No: 204G1A0562
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 30-08-2022
+   Date of Execution: 30-08-2022
+/*
+#include <stdio.h>
+    int main(){
+	printf("Hello World\n");
+  }
