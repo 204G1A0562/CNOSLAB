abced42eb4d7ab554e6fa60a585abf32694cb101
204G1A0562 <94514467+204G1A0562@users.noreply.github.com>
2022-08-30 12:44:15 +0530

    Sample.c
    " T.Muheet Created Sample.c"

---
+++ OSLAB/sample.c
@@ -0,0 +1,10 @@
+/* Name Of the Candidate: L. Ashok
+   Roll No: 204G1A0501
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 29-08-2022
+   Date of Execution: 29-08-2022
+*/
+#include <stdio.h>
+    int main(){
+	printf("Hello World\n");
+ }
