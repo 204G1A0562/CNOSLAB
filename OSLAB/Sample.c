5a28a9422040d4ad307f8d2848bc8805129ee0d4
204G1A0562 <94514467+204G1A0562@users.noreply.github.com>
2022-10-11 10:56:24 +0530

    Update Sample.c

--- OSLAB/Sample.c
+++ OSLAB/Sample.c
@@ -2,8 +2,8 @@
 Name Of the Candidate: T.Muheet Ur Rahman
    Roll No: 204G1A0562
    Title of the Experiment: CPU Scheduling Using FCFS Algorithm
-   Date of Creation: 30-08-2022
-   Date of Execution: 30-08-2022
+   Date of Creation: 11-10-2022
+   Date of Execution: 11-10-2022
 /*
 #include <stdio.h>
     int main(){
