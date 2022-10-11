6ada3bd0b0ce22cd1f7f7d019c163e1381f9ab71
204G1A0562 <94514467+204G1A0562@users.noreply.github.com>
2022-10-11 10:55:08 +0530

    Update sjf.c

--- OSLAB/sjf.c
+++ OSLAB/sjf.c
@@ -1,3 +1,9 @@
+* Name Of the Candidate: T.Muheet ur rahman
+   Roll No: 204G1A0562
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 29-08-2022
+   Date of Execution: 29-08-2022
+*/
 #include<stdio.h>
 void main()
 {
